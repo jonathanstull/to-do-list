3e4e401d1a3e9d07e18b2371501ac50bbc4909aa
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-05-05 07:45:39 -0700

    add and pass fifth test, GetAll returns items

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -50,5 +50,19 @@ namespace ToDoList.TestTools{
 
       CollectionAssert.AreEqual(newList, result);
     }
+
+    [TestMethod]
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      string description01 = "Walk the dog.";
+      string description02 = "Do the dishes.";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+      List<Item> newList = new List<Item> { newItem1, newItem2 };
+
+      List<Item> result = Item.GetAll();
+
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
