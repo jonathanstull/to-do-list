1da9e6ad4f7a8317860220cce78ce876223cdb63
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-05-04 20:30:05 -0700

    add second test, SetDescription

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -21,7 +21,15 @@ namespace ToDoList.TestTools{
       Assert.AreEqual(description, result);
     }
 
-    
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+      string updatedDescription = "Do the dishes.";
+      newItem.Description = updatedDescription;
+      string result = newItem.Description;
     }
   }
 }
